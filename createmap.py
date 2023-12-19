2d90ecc0084043f4bb203d87f933da5c11182517
rehaabsyed <rehaab.syed8257@gmail.com>
2023-12-19 17:54:46 +0800

    removed stuff with just matplotlib (no geopandas)

--- createmap.py
+++ createmap.py
@@ -12,54 +12,7 @@ import geopandas as gpd
 T1_path = r"C:\Users\chand\Desktop\ICPxWP\Sample_data\T1\Legislated_Lands_and_Waters_DBCA_011_WA_GDA2020_Public_Shapefile\Legislated_Lands_and_Waters_DBCA_011.shp"
 T2_path = r"C:\Users\chand\Desktop\ICPxWP\Sample_data\T2\Clearing_Regs_Enviro_Sensitive_Areas_DWER_046_WA_GDA2020_Public_Shapefile\Clearing_Regs_Enviro_Sensitive_Areas_DWER_046.shp"
 
-'''
-#read shape file
-sf = shp.Reader(shp_path)
-
-#convert shp -> pandas df
-def read_shapefile(sf):
-    #fetching the headings from the shape file
-    fields = [x[0] for x in sf.fields][1:]
-
-    #fetching the records from the shape file
-    records = [list(i) for i in sf.records()]
-    shps = [s.points for s in sf.shapes()]
-
-    #converting shapefile data into pandas dataframe
-    df = pd.DataFrame(columns=fields, data=records)
-
-    #assigning the coordinates
-    df = df.assign(coords=shps)
-
-    return df
-'''
-
 # PLOTTING
-'''
-def plot_map(sf, x_lim = None, y_lim = None, figsize = (11,9)):
-    plt.figure(figsize = figsize)
-    id=0
-    for shape in sf.shapeRecords():
-        x = [i[0] for i in shape.shape.points[:]]
-        y = [i[1] for i in shape.shape.points[:]]
-        plt.plot(x, y, 'k')
-        
-        # if (x_lim == None) & (y_lim == None):
-        #     x0 = np.mean(x)
-        #     y0 = np.mean(y)
-        #     plt.text(x0, y0, id, fontsize=10)
-        id = id+1
-    
-    if (x_lim != None) & (y_lim != None):     
-        plt.xlim(x_lim)
-        plt.ylim(y_lim)
-    plt.show()
-
-#calling the function and passing required parameters to plot the full map
-#plot_map(sf)
-'''
-
-# With geopandas
 T1_df = gpd.read_file(T1_path)
 T2_df = gpd.read_file(T2_path)
 
@@ -69,7 +22,6 @@ T1_df.plot(ax=ax, color='blue')
 T2_df.plot(ax=ax, color='red')
 plt.legend()
 plt.show()
-
 
 
 # BUFFERING 
