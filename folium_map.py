16ec559e8927bbd68c682a3f8adfc6378c02648a
rehaabsyed <rehaab.syed8257@gmail.com>
2023-12-21 18:20:30 +0800

    tried base folium map, may be better on jup notebk

--- folium_map.py
+++ folium_map.py
@@ -1,4 +1,11 @@
-# LOADING BASEMAP
+import pandas as pd
+import folium
+
+# LOADING BASEMAP 
+m = folium.Map(location=[37.7749, -122.4194], zoom_start=10)
+m.save('interactive_map.html')
+
+# Open the map in the default web browser
 
 # LOADING CONSTRAINT FILES
 
